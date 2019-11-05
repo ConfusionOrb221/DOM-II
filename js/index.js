5aacdcf211064cbe234824381bfad6280f2617d0
ConfusionOrb221 <nate1898@gmail.com>
2019-11-04 20:19:07 -0500

    finished mvp and stretch

--- js/index.js
+++ js/index.js
@@ -2,8 +2,14 @@
 let atags = document.querySelectorAll("a");
 let main = document.querySelector('.home');
 let body = document.querySelector("body");
+let logo = document.querySelector("h1");
+let paragraph = document.querySelector("p");
+let div = document.querySelector(".destination");
+let p = document.querySelector(".destination p");
+logo.style.userSelect = "none";
 let timer = -1;
 let dragged;
+const tl = new TimelineMax();
 
 atags.forEach(i =>{ 
     i.addEventListener("click", event => {event.preventDefault()}); //prevents reload on anchor tags
@@ -45,6 +51,45 @@ document.addEventListener("drop", function( event ) {
         event.target.style.order = switch2;
     }
 }, false);
+
+window.addEventListener("load", function(){
+    tl.fromTo(logo, 3, {x:-800}, {x:0 , ease: Power2.easeInOut});
+}, false);
+
+logo.addEventListener("dblclick", function(){
+    tl.fromTo(logo, 1.5, {color:"rgba(204,54,60,1)"}, {color:"rgba(0,0,0,1)", ease: Power2.easeIn});
+}, false);
+
+window.addEventListener("copy", function(){
+    window.alert("hey this is annoying xd");
+}, false);
+
+window.addEventListener("resize", function(){
+    if(timer != -1)
+        clearTimeout(timer);
+    timer = window.setTimeout("refreshPage()", 500);
+}, false);
+
+function refreshPage(){
+    document.location.reload();
+}
+
+div.addEventListener("click", function(){
+    div.style.color = "red";
+});
+
+p.addEventListener("click", function(event){
+    event.stopPropagation();
+    p.style.fontSize = "30px";
+});
+
+window.addEventListener("keydown", function(event){
+    if(event.keyCode === 27){
+       window.open("", "", "width=100", "height=100");
+    }
+});
+
+
 
 
 
